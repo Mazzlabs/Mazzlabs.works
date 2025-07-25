1528df73a81af5946620231e6e2bd1b8f5d010d5
Joseph Mazzini <jrogue.mazz@gmail.com>
2025-07-25 10:28:36 -0700

    Fix: Add GameDemosSection back to Home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,6 +3,7 @@ import HeroSection from '../components/sections/HeroSection';
 import AboutSection from '../components/sections/AboutSection';
 import TechStackSection from '../components/sections/TechStackSection';
 import ProjectsSection from '../components/sections/ProjectsSection';
+import GameDemosSection from '../components/sections/GameDemosSection';
 import ContactSection from '../components/sections/ContactSection';
 
 const Home = () => {
@@ -12,6 +13,7 @@ const Home = () => {
       <AboutSection />
       <TechStackSection />
       <ProjectsSection />
+      <GameDemosSection />
       <ContactSection />
     </div>
   );
